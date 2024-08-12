f5c7dd730adb2869d68eb47cbb2ae7885279d7b3
Syed <syednaqvi4345@gmail.com>
2024-08-12 16:55:31 +0100

    added connection

--- src/lambda_functions/extract.py
+++ src/lambda_functions/extract.py
@@ -1,9 +1,16 @@
 import boto3, logging, os
 from datetime import datetime as dt
 from pg8000.native import Connection
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+load_dotenv()
+
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+DATABASE = os.getenv("PG_DATABASE")
+HOST = os.getenv("PG_HOST")
+PORT = os.getenv("PG_PORT")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -11,25 +18,25 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     test_file_path = "./data/test_db.json"
 
+
     s3_client = boto3.client("s3")
     buckets = s3_client.list_buckets()
-    #print(buckets)
+
+    #if statement to check buckets
     for bucket in buckets['Buckets']:
-        #print(f"\n<<< bucket: {bucket}")
+
         if bucket['Name'].startswith("totesys-raw-data-"):
-            #print(f"<<< found")
-            raw_data_bucket_name = bucket['Name']
+            raw_data_bucket = bucket['Name']
             break 
-    print(f"<<< found bucket: {raw_data_bucket_name}")
-           
-    if raw_data_bucket_name:
-        response = s3_client.upload_file(test_file_path, raw_data_bucket_name, "test_db.json")
+    
+    if raw_data_bucket:
+        response = s3_client.upload_file(test_file_path, raw_data_bucket, "test_db.json")
         
         conn = Connection(
-            user = os.getenv("user"),
-            password=os.getenv("password"),
-            database=os.getenv("database"),
-            host=os.getenv("host"),
-            port=os.getenv("port")
+            user= PG_USER,
+            password= PG_PASSWORD,
+            database= DATABASE,
+            host= HOST,
+            port= PORT
         )
         
--- test/test_extract.py
+++ test/test_extract.py
@@ -15,10 +15,7 @@ def aws_credentials():
 def s3(aws_credentials):
     with mock_aws():
         s3 = boto3.client("s3")
-        s3.create_bucket(
-            Bucket="totesys-raw-data-000000",
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
-        )
+
         yield s3
 
 class DummyContext:
@@ -30,9 +27,21 @@ class DummyContext:
 #    assert lambda_handler()
 
 def test_succesfully_upload_json_file(s3):
+    s3.create_bucket(
+    Bucket="totesys-raw-data-000000",
+    CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
     event = {}
     context = DummyContext()
     lambda_handler(event,context)
     listing = s3.list_objects_v2(Bucket="totesys-raw-data-000000")
     assert len(listing["Contents"]) == 1
     assert listing["Contents"][0]["Key"] == "test_db.json"
+
+def test_bucket_does_not_exist(s3): #rewrite
+    event = {}
+    context = DummyContext()
+    lambda_handler(event,context)
+    listing = s3.list_buckets()
+    print(listing)
+
