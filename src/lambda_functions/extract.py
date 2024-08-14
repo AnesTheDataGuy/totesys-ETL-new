c8b336b789f684e596de648fec461f2c439dea2f
Syed <syednaqvi4345@gmail.com>
2024-08-14 10:27:57 +0100

    complete extract func and testing

--- src/lambda_functions/extract.py
+++ src/lambda_functions/extract.py
@@ -2,27 +2,31 @@ import boto3, logging, os, csv
 from datetime import datetime as dt
 from pg8000.native import Connection, Error
 from botocore.exceptions import ClientError
-from dotenv import load_dotenv,  find_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 env_file = find_dotenv(f'.env.{os.getenv("ENV")}')
+
 load_dotenv(env_file)
+
 PG_USER = os.getenv("PG_USER")
 PG_PASSWORD = os.getenv("PG_PASSWORD")
 DATABASE = os.getenv("PG_DATABASE")
 HOST = os.getenv("PG_HOST")
 PORT = os.getenv("PG_PORT")
 
-data_tables = ["sales_order", 
-               "design", 
-               "currency", 
-               "staff", 
-               "counterparty", 
-               "address", 
-               "department", 
-               "purchase_order", 
-               "payment_type", 
-               "payment", 
-               "transaction"]
+data_tables = [
+    "sales_order",
+    "design",
+    "currency",
+    "staff",
+    "counterparty",
+    "address",
+    "department",
+    "purchase_order",
+    "payment_type",
+    "payment",
+    "transaction",
+]
 
 year = dt.now().year
 month = dt.now().month
@@ -34,57 +38,69 @@ second = dt.now().second
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
-    read_file_path_prefix = "./data/"
-    save_file_path_prefix = "./data/test_saved_csv/"
+    save_file_path_prefix = "./data/table_data/"
     s3_client = boto3.client("s3")
     buckets = s3_client.list_buckets()
     found = False
+
     for bucket in buckets["Buckets"]:
         if bucket["Name"].startswith("totesys-raw-data-"):
             raw_data_bucket = bucket["Name"]
             found = True
             break
-    if not found:
-        return "No raw data bucket found"
-    
-    if raw_data_bucket:
-        
-        prefix = f'{year}/{month}/{day}/{hour}-{minute}-{second}/'
 
-        response_upload = s3_client.upload_file(
-            f"{read_file_path_prefix}test_db.csv", raw_data_bucket, "test_db.csv" #2024/08/13/HHMMSS/10csvs
+    if not found:
+        logging.error("No raw data bucket found")
+        return "No raw data bucket found"
+
+    time_prefix = f"{year}/{month}/{day}/{hour}-{minute}-{second}/"
+
+    try:
+        conn = Connection(
+            user=PG_USER,
+            password=PG_PASSWORD,
+            host=HOST,
+            database=DATABASE,
+            port=PORT,
         )
 
-        try:
-            conn = Connection(
-                user=PG_USER, password=PG_PASSWORD, database=DATABASE, port=PORT
-            )
-            for data_table in data_tables:
-                #data type in csv?
-                query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{data_table}';"
-                column_names = conn.run(query)
-                query = f"SELECT * FROM {data_table};"
-                data_rows = conn.run(query)
-                     
-                with open(f"{save_file_path_prefix}test_{data_table}.csv", 'w', newline='') as csvfile:
-                    csvwriter = csv.writer(csvfile)
-                     #Write the column headers
-                    csvwriter.writerow(column_names)
-                     #Write the data rows
-                    csvwriter.writerows(data_rows)
-
-        except Error as e:
-            print(f"Connection to database failed: {e}")
-
-        finally:
-            conn.close()
+        for data_table in data_tables:
+            query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{data_table}';"
+            column_names = conn.run(query)
             
-        return data_rows
-    
+            col_list = []
+            for column in column_names:
+                col_list.append(column[0])
 
-    # Execute a query to fetch all rows from the table
-    #cursor.execute(f"SELECT * FROM {table_name};")
+            query = f"SELECT * FROM {data_table};"
+            data_rows = conn.run(query)
 
-   
+            with open(
+                f"{save_file_path_prefix}{data_table}.csv", "w", newline=""
+            ) as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(col_list)
+                csvwriter.writerows(data_rows)
 
+            try:
+                s3_client.upload_file(
+                        f"{save_file_path_prefix}{data_table}.csv",
+                            raw_data_bucket,
+                            f"{time_prefix}{data_table}.csv")
+                
+            except ClientError as e:
+                logging.error(e)
+                return f"Failed to upload file"
+
+        logging.info(f"Successfully uploaded raw data to {raw_data_bucket}")
+                     
+    except Error as e:
+        logging.error(e['M'])
+        return f"Connection to database failed: {e['M']}"
+
+    finally:
+        conn.close()
+
+    return f"Successfully uploaded raw data to {raw_data_bucket}"
--- test/test_extract.py
+++ test/test_extract.py
@@ -1,11 +1,37 @@
 import pytest, boto3, os
 from moto import mock_aws
 from src.lambda_functions.extract import lambda_handler
+from datetime import datetime as dt
 
+year = dt.now().year
+month = dt.now().month
+day = dt.now().day
+hour = dt.now().hour
+minute = dt.now().minute
+second = dt.now().second
+
+table_data = ['payment_type.csv', 'transaction.csv', 'currency.csv', 'payment.csv', 'sales_order.csv', 'design.csv', 'address.csv', 'counterparty.csv', 'staff.csv', 'department.csv', 'purchase_order.csv']
+data_dir = './data/table_data/'
+
+for table in table_data:
+    if os.path.isfile(f'{data_dir}{table}'):
+        os.remove(f'{data_dir}{table}')
+
+"""
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+PLEASE SWITCH TO TESTING ENV BY:
+export ENV=testing
+IN THE CLI
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+You will need an .env.testing to run tests.
+You will also need to run database/test_db.sql:
+psql -f database/test_db.sql
+Switch back to .env.development to use the ToteSys database.
+"""
 
 @pytest.fixture(scope="function")
 def aws_credentials():
-    """Mocked AWS Credentials for moto."""
+    """Mocked AWS Credentials for S3 bucket."""
     os.environ["AWS_ACCESS_KEY_ID"] = "test"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
     os.environ["AWS_SECURITY_TOKEN"] = "test"
@@ -15,6 +41,7 @@ def aws_credentials():
 
 @pytest.fixture(scope="function")
 def s3(aws_credentials):
+    """Mocked S3 client with raw data bucket."""
     with mock_aws():
         s3 = boto3.client("s3")
         s3.create_bucket(
@@ -23,56 +50,77 @@ def s3(aws_credentials):
         )
         yield s3
 
+
 @pytest.fixture(scope="function")
 def s3_no_buckets(aws_credentials):
+    """Mocked S3 client with no buckets."""
     with mock_aws():
-        s3_nobuckets = boto3.client("s3")       
+        s3_nobuckets = boto3.client("s3")
         yield s3_nobuckets
 
-class DummyContext:
+
+class DummyContext: # Dummy context class used for testing
     pass
 
-@pytest.mark.skip()
-def test_succesfully_upload_json_file(s3):
-    event = {}
-    context = DummyContext()
-    lambda_handler(event, context)
-    listing = s3.list_objects_v2(Bucket="totesys-raw-data-000000")
-    assert len(listing["Contents"]) == 1
-    assert listing["Contents"][0]["Key"] == "test_db.csv"
-
-@pytest.mark.skip()
+@pytest.mark.it("Returns appropriate message if raw data bucket is not found")
 def test_bucket_does_not_exist(s3_no_buckets):
     event = {}
     context = DummyContext()
     expected = "No raw data bucket found"
     assert lambda_handler(event, context) == expected
-    
 
-@pytest.mark.skip()
+
 @pytest.mark.it("script succesfully connects to database")
 def test_succesfully_connects_to_database(s3):
     event = {}
     context = DummyContext()
     assert lambda_handler(event, context) != None
 
+
 @pytest.mark.it("script succesfully writes csv files containing database data to local folder")
 def test_succesfully_save_datatables_to_csv(s3):
-    saved_csv_path = "./data/test_saved_csv/"
-    expected_files = ["test_sales_order.csv", 
-               "test_design.csv", 
-               "test_currency.csv", 
-               "test_staff.csv", 
-               "test_counterparty.csv", 
-               "test_address.csv", 
-               "test_department.csv", 
-               "test_purchase_order.csv", 
-               "test_payment_type.csv", 
-               "test_payment.csv", 
-               "test_transaction.csv"]
+    saved_csv_path = data_dir
+    expected_files = {
+        "sales_order.csv": 0,
+        "design.csv": 0,
+        "currency.csv": 0,
+        "staff.csv": 0,
+        "counterparty.csv": 0,
+        "address.csv": 0,
+        "department.csv": 0,
+        "purchase_order.csv": 0,
+        "payment_type.csv": 0,
+        "payment.csv": 0,
+        "transaction.csv": 0,
+    }
     event = {}
     context = DummyContext()
     lambda_handler(event, context)
     folder_content = os.listdir(saved_csv_path)
     for file in expected_files:
         assert file in folder_content
+
+@pytest.mark.it("Successfully uploads files with correct time stamp key to s3 bucket")
+def test_uploads_csv_to_raw_data_bucket(s3):
+    event = {}
+    context = DummyContext()
+    res = lambda_handler(event, context)
+    listing = s3.list_objects_v2(Bucket="totesys-raw-data-000000")
+    time_prefix = f"{year}/{month}/{day}/{hour}-{minute}-{second}/"
+    assert len(listing["Contents"]) == 11
+    expected_files = {
+        f"{time_prefix}sales_order.csv": 0,
+        f"{time_prefix}design.csv": 0,
+        f"{time_prefix}currency.csv": 0,
+        f"{time_prefix}staff.csv": 0,
+        f"{time_prefix}counterparty.csv": 0,
+        f"{time_prefix}address.csv": 0,
+        f"{time_prefix}department.csv": 0,
+        f"{time_prefix}purchase_order.csv": 0,
+        f"{time_prefix}payment_type.csv": 0,
+        f"{time_prefix}payment.csv": 0,
+        f"{time_prefix}transaction.csv": 0,
+    }
+    for i in range(len(listing)):
+        assert f'{listing["Contents"][i]["Key"]}' in expected_files
+    assert res == f"Successfully uploaded raw data to totesys-raw-data-000000"
