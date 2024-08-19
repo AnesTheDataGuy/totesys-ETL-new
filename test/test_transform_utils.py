cb146e37ccdf1669a6ffdd00c47a5a3def6ad6d4
AnesTheDataGuy <145986383+AnesTheDataGuy@users.noreply.github.com>
2024-08-19 17:02:17 +0100

    Update test_transform_utils.py

--- test/test_transform_utils.py
+++ test/test_transform_utils.py
@@ -60,55 +60,58 @@ def s3(aws_credentials):
         )
         yield s3
 
-
-def test_no_buckets_found(s3_no_buckets):
-
-    result = finds_data_buckets()
-    assert result == 'No buckets found'
+Class TestFindsDataBuckets:
 
 
-def test_no_raw_bucket_found(s3_processed):
-    result = finds_data_buckets()
-    assert result == 'No raw data bucket found'
+    def test_no_buckets_found(self, s3_no_buckets):
+    
+        result = finds_data_buckets()
+        assert result == 'No buckets found'
+    
+    
+    def test_no_raw_bucket_found(self, s3_processed):
+        result = finds_data_buckets()
+        assert result == 'No raw data bucket found'
+    
+    
+    def test_no_processed_bucket_found(self, s3_raw):
+        result = finds_data_buckets()
+        assert result == 'No processed data bucket found'
+    
+    
+    def test_returns_buckets_when_found(self, s3):
+        result = finds_data_buckets()
+        assert result == ('totesys-raw-data-000000','totesys-processed-data-000000')
 
+Class TestConvertToParquet:
 
-def test_no_processed_bucket_found(s3_raw):
-    result = finds_data_buckets()
-    assert result == 'No processed data bucket found'
-
-
-def test_returns_buckets_when_found(s3):
-    result = finds_data_buckets()
-    assert result == ('totesys-raw-data-000000','totesys-processed-data-000000')
-
-
-def test_csv_file_not_found(s3):
-    result = convert_csv_to_parquet('test.csv')
-    assert result == "csv file not found"
-
-def test_convert_csv_to_parquet(s3):
-    s3.put_object(
-        Body="""test,test2,test3
-                1,2,3
-                5,6,7
-                8,9,10""",
-        Bucket='totesys-raw-data-000000',
-        Key='test.csv'
-    )
-    result = convert_csv_to_parquet('test.csv')
-    parquet_buffer = BytesIO(result)
-    df_read_parquet = pd.read_parquet(parquet_buffer)
-    data = {'test': [1,5,8], 'test2': [2,6,9], 'test3': [3,7,10]}
-    df = pd.DataFrame(data)
-
-    assert isinstance(df_read_parquet, pd.DataFrame)
-    assert df.equals(df_read_parquet)
-
-def test_returns_appropriate_message_if_file_is_not_csv(s3):
-    s3.put_object(
-        Body="""Apple""",
-        Bucket='totesys-raw-data-000000',
-        Key='test.txt'
-    )
-    result = convert_csv_to_parquet('test.txt')
-    assert result == "test.txt is not a .csv file."
+    def test_csv_file_not_found(self, s3):
+        result = convert_csv_to_parquet('test.csv')
+        assert result == "csv file not found"
+    
+    def test_convert_csv_to_parquet(self, s3):
+        s3.put_object(
+            Body="""test,test2,test3
+                    1,2,3
+                    5,6,7
+                    8,9,10""",
+            Bucket='totesys-raw-data-000000',
+            Key='test.csv'
+        )
+        result = convert_csv_to_parquet('test.csv')
+        parquet_buffer = BytesIO(result)
+        df_read_parquet = pd.read_parquet(parquet_buffer)
+        data = {'test': [1,5,8], 'test2': [2,6,9], 'test3': [3,7,10]}
+        df = pd.DataFrame(data)
+    
+        assert isinstance(df_read_parquet, pd.DataFrame)
+        assert df.equals(df_read_parquet)
+    
+    def test_returns_appropriate_message_if_file_is_not_csv(self, s3):
+        s3.put_object(
+            Body="""Apple""",
+            Bucket='totesys-raw-data-000000',
+            Key='test.txt'
+        )
+        result = convert_csv_to_parquet('test.txt')
+        assert result == "test.txt is not a .csv file."
