21e8f0ec4e03997772ca9e40bf93027e01a5a0b2
ctrl-shift-ale <alessandro_quaranta@hotmail.com>
2024-08-28 16:42:16 +0100

    update transform again

--- src/lambda_functions/transform.py
+++ src/lambda_functions/transform.py
@@ -1,20 +1,5 @@
 from src.utils.transform_utils import create_star_schema_from_sales_order_csv_file
 
-csvs = [
-    "sales_order.csv",
-    "design.csv",
-    "currency.csv",
-    "staff.csv",
-    "counterparty.csv",
-    "address.csv",
-    "department.csv",
-    "purchase_order.csv",
-    "payment_type.csv",
-    "payment.csv",
-    "transaction.csv",
-]
-
-
 def lambda_handler(event, context):
     """
     This function finds data buckets, converts the csvs to parquet in a star schema format, then uploads this
